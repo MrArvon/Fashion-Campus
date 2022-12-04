047ca1fb4483e341da5ccdc8e345b3160b87d7d5
skulluglify <skulluglify@outlook.com>
2022-12-04 14:07:08 +0700

    AI prefer

--- api/tester.py
+++ api/tester.py
@@ -29,7 +29,7 @@ with sp.Popen(["make", "serve"], stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE) 
         name="user", 
         email="user@gmail.com", 
         phone="080000000001", 
-        password=sqlx_encrypt_pass("admin"), 
+        password=sqlx_encrypt_pass("1234"), 
         type=False, 
         token=None, 
         address=None, 
@@ -234,7 +234,7 @@ with sp.Popen(["make", "serve"], stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE) 
             data['email'] = 'admin@gmail.com' if admin else 'user@gmail.com'
             print(data['email'])
             if do_post(respond, status, '/sign-in', data, "error, wrong password", 400): return sp("Wrong Password")
-            data['password'] = 'admin' # if admin else '1234'
+            data['password'] = 'admin' if admin else '1234'
 
             try:
                 respond, status = post_respond('/sign-in', data)
@@ -444,10 +444,6 @@ with sp.Popen(["make", "serve"], stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE) 
 
             test_signup()
             test_signin() # as User
-
-            test_top_up()
-            test_user_balance()
-
             test_signin(True) # as Admin
 
             test_get_category()
@@ -460,6 +456,8 @@ with sp.Popen(["make", "serve"], stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE) 
             test_update_product()
             test_delete_product()
 
+            test_top_up()
+            test_user_balance()
             test_total_sales()
 
         reset_all_data_test() # CLEARING DATA TEST FIRST
